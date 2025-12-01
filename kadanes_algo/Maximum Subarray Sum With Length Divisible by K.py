f3e8a2b56814cb975ed87a5b5362242a8f4e3699
ravi-aluri <ravialuri2005@gmail.com>
2025-12-01 15:45:18 +0530

    kadanes algo

---
+++ kadanes_algo/Maximum Subarray Sum With Length Divisible by K.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums);
+        prefixSum = [0];
+        for i in range(n):
+            prefixSum.append(nums[i] + prefixSum[-1]);
+        
+        maxSubSum = prefixSum[k];
+        for i in range(0,k):
+            currSum = 0;
+            for j in range(i,n-k+1,k):
+                l = j + k - 1;
+                currSum = max(currSum+prefixSum[l+1]-prefixSum[j],prefixSum[l+1]-prefixSum[j]);
+                maxSubSum = max(currSum,maxSubSum);
+
+        return maxSubSum;
